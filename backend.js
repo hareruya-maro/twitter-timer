27de36ce42b3d0a991a820ce1b1d07b7566edb41
Haruya Nakamura <hareruya.maro@gmail.com>
2019-11-27 01:04:25 +0900

    Tweetdeck対応

--- backend.js
+++ backend.js
@@ -6,7 +6,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     }, function (items) {
         // 機能がONの場合だけ
         if (items.isOn === 'ON') {
-            chrome.tabs.query({ url: "https://twitter.com/*" }, function (tabs) {
+            chrome.tabs.query({ url: "https://*.twitter.com/*" }, function (tabs) {
                 tabs.map((tab) => {
                     chrome.tabs.remove(tab.id);
                 })
